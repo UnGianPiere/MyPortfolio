cb5d8c68ee33cfe2a0ef9393ebbbe01a06ef3bca
GianPiere <74022578@continental.edu.pe>
2025-08-23 20:24:15 -0500

    23/08 - 5

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -124,7 +124,7 @@ export default function Footer() {
                                     <svg className="w-4 h-4" viewBox="0 0 24 24" fill="currentColor">
                                         <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
                                     </svg>
-                                    <span>geanpierehuarcaya@gmail.com</span>
+                                    <span className="truncate">geanpierehuarcaya@gmail.com</span>
                                 </div>
                             </a>
                             <a href="https://wa.me/+51912390600"
